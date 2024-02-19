86cccd170b79add63422992f367df098a1781315
Joshua Chow <thejoshchow@gmail.com>
2024-02-18 21:29:14 -0700

    project cards centered

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -5,10 +5,39 @@ const About = () => {
     <section id="about" className="section row">
       <div className="d-flex align-items-center justify-content-center">
         <div className="col-xl-6 d-flex flex-column align-items-end"></div>
-        <div className="col-xl-6 d-flex flex-column align-items-start">
+        <div
+          className="col-xl-6 d-flex flex-column align-items-start"
+          style={{ paddingRight: "10rem" }}
+        >
           <h1>hello, world</h1>
           <h1>i'm josh</h1>
-          <p>beep boop beep boop</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            euismod non dui sit amet tempus. Vivamus non viverra est. Nulla
+            vehicula magna consectetur, auctor mauris ac, imperdiet lorem. Donec
+            pellentesque auctor nibh, et suscipit est rutrum non. Praesent
+            semper enim tellus, sit amet euismod lacus luctus dignissim. Donec
+            ornare sem ut mauris pulvinar, a convallis lorem faucibus. Donec
+            elementum rutrum libero. Nunc efficitur dolor justo, in luctus diam
+            blandit in. Cras malesuada in elit et viverra. Phasellus eu libero
+            purus. Fusce elementum ut elit sit amet tristique. Nullam id dapibus
+            quam. Curabitur in ornare diam, sit amet volutpat massa. Cras quis
+            tellus nibh.
+          </p>
+        </div>
+      </div>
+      <div className="container pt-5">
+        <div className="d-flex flex-row justify-content-center flex-wrap">
+          <h5 className="tech">react</h5>
+          <h5 className="tech">python</h5>
+          <h5 className="tech">javascript</h5>
+          <h5 className="tech">redux</h5>
+          <h5 className="tech">fastapi</h5>
+          <h5 className="tech">django</h5>
+          <h5 className="tech">vue.js</h5>
+          <h5 className="tech">flask</h5>
+          <h5 className="tech">html</h5>
+          <h5 className="tech">css</h5>
         </div>
       </div>
     </section>
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,45 +2,94 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 
 const Projects = () => {
-  function projectCard(project) {
+  const projectCard = (project) => {
     return (
-      <Card style={{ width: "18rem", margin: "3rem" }}>
-        <Card.Img variant="top" src="" alt="..." fluid />
+      <Card>
+        <Card.Img
+          variant="top"
+          src={
+            project.screenshot === ""
+              ? "img/placeholder.svg"
+              : project.screenshot
+          }
+          alt="..."
+        />
+        <Card.Header className="d-flex justify-content-center">
+          <a className="btn card-button" href={project.repo}>
+            github
+          </a>
+        </Card.Header>
         <Card.Body>
           <Card.Title>{project.name}</Card.Title>
           <Card.Text>{project.description}</Card.Text>
-          <a href={project.repo}>Github</a>
         </Card.Body>
+        <Card.Footer className="d-flex flex-wrap justify-content-center">
+          {project.tech.map((item) => (
+            <div className="badge" key={item}>
+              {item}
+            </div>
+          ))}
+        </Card.Footer>
       </Card>
     );
-  }
+  };
   const projects = [
     {
+      name: "Portfolio",
+      screenshot: "img/portfolio.png",
+      description:
+        "Single-page application built with React and selective styling using Bootstrap and HTML/CSS.",
+      tech: ["Javascript", "React", "HTML", "CSS"],
+      website: "",
+      repo: "https://github.com/thejoshchow/thejoshchow-portfolio",
+    },
+    {
       name: "Travel Buddy",
-      screenshot: "none",
-      description: "Travel Buddy etc...",
-      techStack: ["Python", "Javascript", "React", "Redux"],
+      screenshot: "img/travelbuddy.png",
+      description:
+        "Travel buddy is a web application designed to help facilitate and encourage the the conversation around planning a group trip. The project consists of a postgresql database served by a fastapi server and a react frontend with ui components provided bootstrap components as well as custom css styling. Front-end authentication and authorization is hand-built using redux toolkit and rtk query.",
+      tech: ["Python", "FastAPI", "Javascript", "React", "Redux", "CSS"],
       website: "",
       repo: "",
     },
     {
       name: "CarCar",
-      screenshot: "none",
+      screenshot: "",
       description: "CarCar...",
-      techStack: [""],
+      tech: ["Python", "Django", "React"],
       website: "",
       repo: "",
     },
-    {},
+    {
+      name: "",
+      screenshot: "",
+      description: "",
+      tech: [],
+      website: "",
+      repo: "",
+    },
+    {
+      name: "",
+      screenshot: "",
+      description: "",
+      tech: [],
+      website: "",
+      repo: "",
+    },
   ];
   return (
     <section id="projects" className="section row">
-      {/* <div className="container d-flex justify-content-center align-items-center"></div> */}
-      <div className="container d-flex flex-column justify-content-center align-items-center">
-        <h3>projects</h3>
-        <div className="d-flex justify-content-center align-items-center flex-wrap">
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <div className="container">
+          <h3 className="m-auto">projects</h3>
+        </div>
+        <div className="container d-flex flex-wrap p-5">
           {projects.map((project) => {
-            return projectCard(project);
+            return (
+              <div key={project.name} className="col-12 col-xl-3 m-5">
+                {projectCard(project)}
+              </div>
+            );
           })}
         </div>
       </div>
